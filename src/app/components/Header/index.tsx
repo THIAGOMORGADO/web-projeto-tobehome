468c649e41bf630b9bcfd9cec446e0506c89c417
thiago morgado <thiago_morgado@hotmail.com>
2025-01-06 18:44:50 -0300

    melhorias no ui e ux

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -11,63 +11,95 @@ export default function Header() {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <header className="bg-[#341A72] text-white shadow-sm">
-      <nav className="container mx-auto sm:px-24 px-6 py-3">
+    <header className="bg-gradient-to-r from-purple-800 to-indigo-800 text-white shadow-lg">
+      <nav className="container mx-auto px-8 py-5">
         <div className="flex justify-between items-center">
-          <div className="text-2xl font-bold">To Be Home</div>
-          <div className="hidden md:flex items-center justify-between flex-grow ml-6">
-            <div className="flex items-center justify-center">
-              <Link href="#" className="text-white hover:text-[#fff] mx-4">
+          <div className="text-4xl font-bold tracking-tight">To Be Home</div>
+          <div className="hidden md:flex items-center justify-between flex-grow ml-8">
+            <div className="flex items-center justify-center space-x-8">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 transition-colors duration-200"
+              >
                 Início
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] mx-4">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 transition-colors duration-200"
+              >
                 Imóveis
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] mx-4">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 transition-colors duration-200"
+              >
                 Serviços
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] mx-4">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 transition-colors duration-200"
+              >
                 Contato
               </Link>
             </div>
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-6">
               <Button
                 variant="outline"
-                className=" text-[#341A72] border border-[#341A72]"
+                className="text-white border border-white bg-black"
               >
                 Fazer login
               </Button>
-              <Button variant="secondary">Reservar Agora</Button>
+              <Button variant="secondary" className="bg-black text-white">
+                Reservar Agora
+              </Button>
             </div>
           </div>
           <button className="md:hidden" onClick={toggleMenu}>
             {isMenuOpen ? (
-              <X size={24} color="#fff" />
+              <X size={28} color="#fff" />
             ) : (
-              <Menu size={24} color="#fff" />
+              <Menu size={28} color="#fff" />
             )}
           </button>
         </div>
         {isMenuOpen && (
-          <div className="md:hidden mt-4">
-            <div className="flex flex-col space-y-2">
-              <Link href="#" className="text-white hover:text-[#fff] py-2">
+          <div className="md:hidden mt-5">
+            <div className="flex flex-col space-y-3">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 py-3 transition-colors duration-200"
+              >
                 Início
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] py-2">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 py-3 transition-colors duration-200"
+              >
                 Imóveis
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] py-2">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 py-3 transition-colors duration-200"
+              >
                 Serviços
               </Link>
-              <Link href="#" className="text-white hover:text-[#fff] py-2">
+              <Link
+                href="#"
+                className="text-white hover:text-indigo-300 py-3 transition-colors duration-200"
+              >
                 Contato
               </Link>
-              <div className="flex flex-col space-y-4 mt-2">
-                <Button variant="outline" className="border-white text-black">
+              <div className="flex flex-col space-y-5 mt-3">
+                <Button
+                  variant="outline"
+                  className="border-white text-white bg-black"
+                >
                   Login
                 </Button>
-                <Button variant="outline" className="border-white text-black">
+                <Button
+                  variant="outline"
+                  className="border-white text-white bg-black"
+                >
                   Reservar Agora
                 </Button>
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <main></main>
+      <main>
+        <HeroSection />
+      </main>
     </>
   );
 }
