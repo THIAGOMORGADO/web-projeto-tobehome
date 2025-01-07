b4c643ae62a764fef95a3f7dd3b07330e9340c0c
thiago morgado <thiago_morgado@hotmail.com>
2025-01-07 11:26:52 -0300

    Atualização do use Toast

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -22,7 +22,7 @@ export default function SignUp() {
   const [error, setError] = useState("");
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-[#030142]">
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle className="text-2xl font-bold">Criar Conta</CardTitle>
