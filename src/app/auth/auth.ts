9fd51ee274c4933c76d748a54d6f5ef968f7733f
thiago morgado <thiago_morgado@hotmail.com>
2025-01-08 18:22:12 -0300

    atualizando error

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,26 +1,5 @@
-"use server";
+import NextAuth from "next-auth";
 
-import { redirect } from "next/navigation";
-
-export async function signIn(formData: FormData) {
-  const email = formData.get("email");
-  const password = formData.get("password");
-
-  // TODO: Implement your authentication logic here
-  console.log("Sign in attempt:", { email, password });
-
-  // For now, we'll just redirect to a success page
-  redirect("/dashboard");
-}
-
-export async function signUp(formData: FormData) {
-  const name = formData.get("name");
-  const email = formData.get("email");
-  const password = formData.get("password");
-
-  // TODO: Implement your account creation logic here
-  console.log("Sign up attempt:", { name, email, password });
-
-  // For now, we'll just redirect to a success page
-  redirect("/dashboard");
-}
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [],
+});
