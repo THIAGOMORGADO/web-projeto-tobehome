982f044c83068b1a8a7625ff973189a6bf7213db
Thiago morgado <thiago_morgado@hotmail.com>
2025-01-15 14:57:56 -0300

    atualizando repositorio

--- src/app/firebaseServices/Firebase.ts
+++ src/app/firebaseServices/Firebase.ts
@@ -5,11 +5,6 @@ import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCat56-zCYaw3el73BirGhCbtfhN5a3zh8",
   authDomain: "notificaiton-todohome.firebaseapp.com",
--- src/app/firebaseServices/firebaseDateBase/index.ts
+++
@@ -1,6 +0,0 @@
-import { getDatabase } from "firebase/database";
-import firebaseApp from "./firebaseConfig";
-
-// Inicializa o Realtime Database
-const db = getDatabase(firebaseApp);
-export default db;
