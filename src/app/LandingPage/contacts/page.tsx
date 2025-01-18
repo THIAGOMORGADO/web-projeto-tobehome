6e588c1da7dc0187b20aebb1d26bf7f9ba36d7a0
Thiago morgado <thiago_morgado@hotmail.com>
2025-01-17 21:24:13 -0300

    ataulizando Tabalhe conosco

--- src/app/LandingPage/contacts/page.tsx
+++ src/app/LandingPage/contacts/page.tsx
@@ -1,13 +1,10 @@
 import Header from "@/app/components/Header";
 import React from "react";
-import TrabalheConosco from "../components/workforus";
 
 export default function page() {
   return (
     <div>
       <Header />
-
-      <TrabalheConosco />
     </div>
   );
 }
