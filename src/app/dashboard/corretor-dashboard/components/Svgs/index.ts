e1f2131396a9c0a48c08bc1f0955adf0c4267187
Thiago morgado <thiago_morgado@hotmail.com>
2025-01-20 13:01:04 -0300

    atualizando detalhes do cadastro

--- src/app/dashboard/corretor-dashboard/components/Svgs/index.ts
+++ src/app/dashboard/corretor-dashboard/components/Svgs/index.ts
@@ -1,5 +1,5 @@
 import PessoaIcon from "@/app/dashboard/corretor-dashboard/components/Svgs/assets/pepplos.jpg";
-import BAthSuit from "../Svgs/assets/bathANdbardrpw.jpg";
+import BAthSuit from "../Svgs/assets/bathANdbardrpw.png";
 import Suite from "../Svgs/assets/suites.jpg";
 import banheiroSuite from "../Svgs/assets/bathrowAndSUite.jpg";
 
